cb4826d97e99e373d7e4f96ca6674911223cc706
isourabh-hub <sourabhray2018@gmail.com>
2025-07-06 07:10:30 +0000

    let, var and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,45 @@
+const accountId = 112244
+let accountEmail = "sk@gmail.com";
+var accountPassword = "12345"
+accountCity = "Raipur"
+
+// accountId = 124512 this can not be allowed
+
+console.log(accountId);
+
+accountEmail = "sr@gmail.com"
+accountCity ="Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+function variableDemo() {
+  var userName = "Rahul";
+  var userName = "Sourabh"; //  Re-declared (allowed)
+  console.log("var userName:", userName); 
+
+
+  let userAge = 25;
+  // let userAge = 30; // Error: Cannot re-declare 'userAge'
+  userAge = 30; // ✅ Can re-assign
+  console.log("let userAge:", userAge); 
+
+
+  const userCity = "Mumbai";
+  // userCity = "Delhi"; // Error: Cannot re-assign a const
+  console.log("const userCity:", userCity); 
+
+ 
+  if (true) {
+    var x = 100;
+    let y = 200;
+    const z = 300;
+  }
+
+  console.log("var x:", x); // Output: 100 (var is function scoped)
+  // console.log("let y:", y); // Error: y is not defined
+  // console.log("const z:", z); // Error: z is not defined
+}
+
+variableDemo();
+
