7e93dc24701c094f0d14502f6122247f46c03d02
isourabh-hub <sourabhray2018@gmail.com>
2025-07-06 16:55:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber);
+
+let undefinedEx = undefined
+
+// console.log(typeof(undefinedEx))
+
+let undefinedNumber = Number(undefinedEx)
+// console.log(typeof undefinedNumber )
+// console.log(undefinedNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let isLoggedIn = 1
+
+let booleanData = Boolean(isLoggedIn)
+
+console.log( booleanData)
+//"sourabh" => true
+//1 => true, 0 => false
+//"" => false
+
+let someNumber = 1245
+
+let intoString = String(someNumber)
+
+console.log(intoString);
+console.log(typeof intoString);
