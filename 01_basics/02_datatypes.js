f79f7408c2ddb1f29adeb13406baa3fbc0f7fe6f
isourabh-hub <sourabhray2018@gmail.com>
2025-07-06 08:31:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all js code as newer version of js 
+
+// alert(2+2)  //we are using node , not browser so this only work in the browser
+
+let name="sourabh" 
+let age = 14 
+let LoggedIn = true 
+
+//bigInt
+//string =>""
+//integer => number
+//boolean =>true/false
+//null => standlone vakue
+//undefined => no values
+//symbol => uniqueness
+
+//object 
+
+console.log(typeof "sourabh");
+console.log(typeof LoggedIn);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
