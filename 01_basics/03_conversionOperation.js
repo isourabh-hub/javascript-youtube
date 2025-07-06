87b8986af23862b02ee6502d32e80f04d188afff
isourabh-hub <sourabhray2018@gmail.com>
2025-07-06 17:30:32 +0000

    operation of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,7 @@ let isLoggedIn = 1
 
 let booleanData = Boolean(isLoggedIn)
 
-console.log( booleanData)
+// console.log( booleanData)
 //"sourabh" => true
 //1 => true, 0 => false
 //"" => false
@@ -32,5 +32,48 @@ let someNumber = 1245
 
 let intoString = String(someNumber)
 
-console.log(intoString);
-console.log(typeof intoString);
+// console.log(intoString);
+// console.log(typeof intoString);
+
+
+//********************************   Operation   ****************************
+
+let value = 4
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Sourabh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(3 + "3");
+// console.log(3 + Number("3"));
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+
+// console.log(gameCounter)
+
+//Postfix example
+let x = 4
+const y = x++
+
+console.log(`x: ${x}, y:${y}`)
+
+//prefix example
+let a = 4
+const b = ++a
+
+console.log(`a: ${a}, b:${b}`)
